a8035c4ad63153adda5df96ba8074a0effe6fed4
Daniel Fernández Bernal <fernandez.bedan20@triana.salesianos.edu>
2021-09-29 13:31:08 +0200

    Clase

---
+++ src/main/java/com/salesianostriana/equipo/ejercicio04/Alumno.java
@@ -0,0 +1,39 @@
+package com.salesianostriana.equipo.ejercicio04;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Alumno {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String telefono;
+    private String email;
+
+    @ManyToOne
+    private Direccion direccion;
+
+    @ManyToOne
+    private Curso curso;
+
+    public Alumno(String nombre, String apellido1, String apellido2, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.telefono = telefono;
+        this.email = email;
+    }
+}
+
